36fb4a60065a2dad3f45fbf648a0ed62d32c60d5
Tomasz Czarnecki <tomasz@czarnecki.uk>
2018-05-30 12:15:15 +0200

    Added password hashing

--- src/main/java/pl/nabuhodonozo/twitter/entity/User.java
+++ src/main/java/pl/nabuhodonozo/twitter/entity/User.java
@@ -7,8 +7,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
+import org.mindrot.jbcrypt.BCrypt;
 
 @Entity
 @Table(name = "user")
@@ -49,7 +51,7 @@ public class User {
 		return password;
 	}
 	public void setPassword(String password) {
-		this.password = password;
+		this.password = BCrypt.hashpw(password, BCrypt.gensalt(5));
 	}
 	public String getEmail() {
 		return email;
